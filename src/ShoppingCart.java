8d24d108656eb3bb7fa015153278f7cb9e347968
tv0823 <tv0823@bs.amalnet.k12.il>
2024-10-26 14:54:15 +0300

    this added

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,16 +1,14 @@
 public class ShoppingCart {
     private Product[] productList;
-    private int index;
 
     public ShoppingCart() {
         this.productList = new Product[10];
-        index = 0;
     }
 
     public double calculateTotal()
     {
         double sum = 0.0;
-        for(Product p : productList)
+        for(Product p : this.productList)
         {
             sum += p.getPrice();
         }
